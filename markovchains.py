c7fbe1e5e38a2b46d9a64949b3e52dc5266ab843
Ruben de Vries <r.devries6@students.uu.nl>
2020-05-21 17:16:22 +0200

    Created an object-oriented structure and tried to add memory as parameter

--- markovchains.py
+++ markovchains.py
@@ -1,65 +1,86 @@
 import numpy as np
 
 
-def add_alphabetically(lst, word):
+def insert(lst, inserted):
     ''' Add word in lst in case that it does not exist in lst yet,
         and such that the alphabetical order is respected. '''
 
     for index, item in enumerate(lst):
-        if item == word:
+        if item == inserted:
             return lst
-        elif item > word:
-            return lst[:index] + [word] + lst[index:]
+        elif item > inserted:
+            return lst[:index] + [inserted] + lst[index:]
 
-    return lst + [word]
-
-
-def get_couples(text):
-    '''
-    '''
-
-    words_by_index = [""]
-    couples = {}
-    splitted_text = text.split()
-
-    for word in splitted_text:
-        words_by_index = add_alphabetically(words_by_index, word)
-
-    for index, word in enumerate(words_by_index):
-        couples[word] = []
-
-    last_word = ""
-    for word in splitted_text:
-        couples[last_word].append(word)
-
-        if word[-1] == "." or word[-1] == "?" or word[-1] == "!":
-            couples[word].append("")
-            last_word = ""
-        else:
-            last_word = word
-
-    return couples
+    return lst + [inserted]
 
 
-def generate_sentence(couples):
-    '''
+class MarkovTextChain:
+    ''' Represents a Markov chain that simulates text.
+        This is done by giving the object an example text from which it uses
+        the conditional probabilities of a word occuring in a sentence given
+        the words that are before it.
     '''
 
-    chain = []
-    last_word = ""
+    def __init__(self, example_text, memory):
+        ''' Initialise object
+        '''
+        self.example_text = example_text.split()
+        self.memory = memory
 
-    while last_word == "" or (last_word[-1] != "." and last_word[-1] != "?" and \
-        last_word[-1] != "!"):
+        self.list_nodes()
+        self.calc_probabilities()
 
-        last_word = np.random.choice(couples[last_word])
-        chain.append(last_word)
+    def list_nodes(self):
+        ''' Creates an alphabetically ordered list of all nodes occuring in the
+        example text. Nodes are tuples of words with length determined by
+        memory.
+        '''
+        nodes = [""] # The empty string represents the beginning of a sentence.
 
-    return " ".join(chain)
+        for i in range(len(self.example_text) - self.memory):
+            node = ""
+            for j in range(self.memory):
+                node += self.example_text[i + j] + " "
+                nodes = insert(nodes, node) # Adds node to nodes alphabetically
+
+        self.nodes = nodes
+
+    def calc_probabilities(self):
+        '''
+        '''
+        next_words = {}
+
+        for node in self.nodes:
+            next_words[node] = []
+
+        last_node = ""
+        for word in self.example_text:
+            next_words[last_node].append(word + " ")
+            last_node = last_node[last_node.find(" ") + 1:] + word +  " "
+
+            if word[-1] == "." or word[-1] == "!" or word[-1] == "?":
+                next_words[last_node] = [""]
+                last_node = ""
+
+        self.next_words = next_words
+
+    def generate_sentence(self):
+        ''' Generate a sentence.
+        '''
+        last_node = np.random.choice(self.next_words[""])
+        chain = [last_node]
+
+        while last_node != "":
+            last_node = np.random.choice(self.next_words[last_node])
+            chain.append(last_node)
+
+        return "".join(chain)
 
 
-text = open("Trump Speech.txt").read()
-couples = get_couples(text)
-for i in range(10):
-    print(generate_sentence(couples))
+text = open("Trump_Speech.txt").read()
+trumpchain = MarkovTextChain(text, 1)
+
+for i in range(20):
+    print(trumpchain.generate_sentence())
 
 
